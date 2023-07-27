c5943214f9c6a266db3d40c964065eeb5a0a9584
snake324 <kristi4n90@gmail.com>
2023-07-27 09:00:17 +0200

    springboot installed

---
+++ Backend/src/main/java/dev/kristi4n90/aniverse/AniverseApplication.java
@@ -0,0 +1,13 @@
+package dev.kristi4n90.aniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniverseApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/dev/kristi4n90/aniverse/AniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kristi4n90.aniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
