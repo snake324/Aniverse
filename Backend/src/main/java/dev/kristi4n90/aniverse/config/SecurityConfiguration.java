b5eb5fdfd1f7374573b63ca7252f54d0946de1e4
snake324 <kristi4n90@gmail.com>
2023-09-11 20:54:08 +0200

    users, youtube links updated

--- Backend/src/main/java/dev/kristi4n90/aniverse/config/SecurityConfiguration.java
+++ Backend/src/main/java/dev/kristi4n90/aniverse/config/SecurityConfiguration.java
@@ -5,14 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -25,10 +28,14 @@ public class SecurityConfiguration {
 
         http.cors(withDefaults())
                 .csrf(csfr -> csfr.disable())
-                .formLogin(form -> form.disable())
+                //.formLogin(form -> form.disable())
                 .logout(out -> out
                         .logoutUrl("/logout")
                         .deleteCookies("JSESSIONID"))
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/register", "/anime/**").permitAll()
+                    .requestMatchers("/user").hasRole("USER")
+                    .anyRequest().authenticated())
                 .httpBasic(withDefaults())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
@@ -47,7 +54,25 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails luffy = User.builder()
+                .username("luffy")
+                .password("{bcrypt}$2a$12$SnIc0lskG/qyHSTWT8vpNOVQyaN5XxO2peQ8mqrepLv41vRv/GHhe")
+                .roles("USER")
+                .build();
+
+        UserDetails cristian = User.builder()
+                .username("cristian")
+                .password("{bcrypt}$2a$12$S1MB85LjsOxWffqdKo31u.AboEK8MIhKduOjEam3e1n.BCHBENfMW")
+                .roles("ADMIN")
+                .build();
+
+        Collection<UserDetails> users = new ArrayList<>();
+                users.add(luffy);
+                users.add(cristian);
+
+        return new InMemoryUserDetailsManager(users);
+
     }
 }
