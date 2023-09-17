388855dceb1e07f92abdbbd7293e0c37755b5fe5
snake324 <kristi4n90@gmail.com>
2023-09-17 17:35:25 +0200

    crud users

--- Backend/src/main/java/dev/kristi4n90/aniverse/controllers/UserController.java
+++ Backend/src/main/java/dev/kristi4n90/aniverse/controllers/UserController.java
@@ -1,11 +1,16 @@
 package dev.kristi4n90.aniverse.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import dev.kristi4n90.aniverse.models.User;
-import dev.kristi4n90.aniverse.repositories.UserRepository;
+import dev.kristi4n90.aniverse.services.UserService;
 
 import java.util.List;
 
@@ -14,11 +19,28 @@ import java.util.List;
 public class UserController {
     
     @Autowired
-    UserRepository repository;
+    UserService service;
 
     @GetMapping
-    public List<User> index() {
-        List<User> users = repository.findAll();
-        return users;
+    public List<User> getAllUsers(){
+        return service.getAllUsers();
     }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id){
+        return service.getUserById(id);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id){
+        User userToUpdate = service.getUserById(id);
+        service.updateUser(userToUpdate);
+        return ResponseEntity.ok(userToUpdate);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteUser(@RequestBody User user){
+        service.deleteUser(user);
+    }
+
 }
--- Backend/src/main/java/dev/kristi4n90/aniverse/services/UserService.java
+++ Backend/src/main/java/dev/kristi4n90/aniverse/services/UserService.java
@@ -1,8 +1,11 @@
 package dev.kristi4n90.aniverse.services;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +16,23 @@ import dev.kristi4n90.aniverse.repositories.UserRepository;
 @Service
 public class UserService {
     
+    @Autowired
     UserRepository userRepository;
+
+    @Autowired
     RoleService roleService;
 
-    public UserService(UserRepository userRepository, RoleService roleService) {
-        this.userRepository = userRepository;
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        Optional<User> opt = userRepository.findById(id);
+        if (opt.isPresent()) {
+            return opt.get();
+        } else {
+            return null;
+        }
     }
 
     public User save(User user) throws Throwable {
@@ -37,6 +52,14 @@ public class UserService {
         roles.add(defauRole);
 
         user.setRoles(roles);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(User user) {
+        userRepository.delete(user);
     }
 
 }
